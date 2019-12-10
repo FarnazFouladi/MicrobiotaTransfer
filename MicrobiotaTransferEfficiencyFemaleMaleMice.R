71ba8b23b930d68b18eb1f22138187a8d511eac9
Farnaz <ffouladi@uncc.edu>
2019-12-10 12:01:53 -0500

    First commit

---
+++ MicrobiotaTransferEfficiencyFemaleMaleMice.R
@@ -0,0 +1,189 @@
+#Farnaz Fouladi
+#11-22-19
+#Comparing the transfer efficiency between male and female mice. 
+#Comparison of percent of shared, only in slurries, only in mice between female and male mice.
+
+rm(list =ls())
+
+output<-"/Users/farnazfouladi/Google Drive/MicrobiotaTransfer/output/"
+input<-"/Users/farnazfouladi/Google Drive/MicrobiotaTransfer/input/"
+setwd(output)
+source("/Users/farnazfouladi/Google Drive/MicrobiotaTransfer/Rcode/pie2.R")
+data<-read.table(paste0(output,"svMeta.txt"),sep = "\t",header=TRUE,check.names=FALSE,na.strings = "NA" ,comment.char="")
+finishAbundanceIndex<-which(colnames(data)=="Sample")-1
+#Removing low abundant sv
+sv<-data[,1:finishAbundanceIndex]
+sv<-sv[,colMeans(sv>0)>=0.1]
+myT<-cbind(sv,data[,(finishAbundanceIndex+1):ncol(data)])
+finishAbundanceIndex<-which(colnames(myT)=="Sample")-1
+Donors<-as.character(myT$Donor[myT$Sample.type=="Human.donor"])
+myTH<-myT[myT$Sample.type=="Fecal.slurry",]
+myTMouse<-myT[myT$Sample.type=="Mouse.feces",]
+
+#Choose female or male mice
+sex="F"
+myTMouse_S<-myTMouse[myTMouse$Sex==sex,]
+
+Slurries<-levels(factor(myTH$Slurry.ID1))
+#Removing slurries that were not used for colonization of mice
+Slurries_S<-Slurries[Slurries %in% as.character(myTMouse_S$Slurry.ID1)]
+bugs<-c("TotalbugSlurry","bugShared","bugSlurry","bugMouse","bugNogroup")
+df<-data.frame(bugs)
+
+
+for (week in 1:4){
+  myTM<-myTMouse_S[myTMouse_S$Week==week,]
+  TotalbugSlurry<-0
+  bugShared<-0
+  bugSlurry<-0
+  bugMouse<-0
+  bugNogroup<-0
+  numMouse<-0
+  
+  for (slurry in Slurries_S){
+    
+    if (slurry %in% myTM$Slurry.ID1){
+    
+    myTHS<-myTH[myTH$Slurry.ID1==slurry,]
+    myTMS<-myTM[myTM$Slurry.ID1==slurry,]
+    for (j in 1:nrow(myTMS)){
+      #print(nrow(myTMS))
+      numMouse<-numMouse+1
+      bugM<-myTMS[j,]
+      for (i in 1:finishAbundanceIndex){
+        bugH<-myTHS[,i]
+        bugM_individual<-bugM[,i]
+        if (bugH>0){
+          TotalbugSlurry<-TotalbugSlurry+1 
+          if (bugM_individual>0){
+            bugShared<-bugShared+1
+          }else{
+            bugSlurry<-bugSlurry+1
+          }
+        }else {
+          if (bugM_individual>0){
+            bugMouse<-bugMouse+1
+          }else{
+            bugNogroup<-bugNogroup+1
+          }
+        }
+      }
+      
+      value<-c(TotalbugSlurry,bugShared,bugSlurry,bugMouse,bugNogroup)
+      df<-cbind(df,value)
+      colnames(df)[which(colnames(df)=="value")]<-paste0(slurry,"week ",week,"_",bugM$Sample.ID) #dim(df)5 536
+      bugShared<-0
+      bugSlurry<-0
+      bugMouse<-0
+      bugNogroup<-0
+      TotalbugSlurry<-0
+    }
+  }
+  }
+  print(paste0("Num of mice at week ",week," ",numMouse))
+}
+#How much percentage of total slurry bug is shared with mouse and how much not.
+totalBuginSlurry<-as.numeric(df[1,2:ncol(df)])
+twoGroups<-sweep(df[2:3,2:ncol(df)],2,totalBuginSlurry,'/')*100
+# Calculating the percentage of bugShared, bugSlurry,bugMouse
+sum<-colSums(df[2:4,2:ncol(df)])
+threeGroups<-sweep(df[2:4,2:ncol(df)],2,sum,'/')*100
+df1<-rbind(df[,2:ncol(df)],twoGroups,threeGroups)
+rownames(df1)<-c("TotalbugSlurry","bugShared","bugSlurry","bugMouse","bugNogroup","percentBugShared","percentBugSlurry","percentBugShared3","percentBugSlurry3","percentBugMouse3")
+
+weeks<-sapply(colnames(df1),function(x){strsplit(x," ")[[1]][2]})
+weeksN<-sapply(weeks,function(x){strsplit(x,"_")[[1]][1]})
+
+meanWeek1<-as.vector(rowMeans(df1[1:which(weeksN=="1")[length(which(weeksN=="1"))]]))
+meanWeek2<-as.vector(rowMeans(df1[(which(weeksN=="1")[length(which(weeksN=="1"))]+1):which(weeksN=="2")[length(which(weeksN=="2"))]]))
+meanWeek3<-as.vector(rowMeans(df1[(which(weeksN=="2")[length(which(weeksN=="2"))]+1):which(weeksN=="3")[length(which(weeksN=="3"))]]))
+meanWeek4<-as.vector(rowMeans(df1[(which(weeksN=="3")[length(which(weeksN=="3"))]+1):which(weeksN=="4")[length(which(weeksN=="4"))]]))
+
+sd1<-apply(df1[1:which(weeksN=="1")[length(which(weeksN=="1"))]],1,sd)
+sd2<-apply(df1[(which(weeksN=="1")[length(which(weeksN=="1"))]+1):which(weeksN=="2")[length(which(weeksN=="2"))]],1,sd)
+sd3<-apply(df1[(which(weeksN=="2")[length(which(weeksN=="2"))]+1):which(weeksN=="3")[length(which(weeksN=="3"))]],1,sd)
+sd4<-apply(df1[(which(weeksN=="3")[length(which(weeksN=="3"))]+1):which(weeksN=="4")[length(which(weeksN=="4"))]],1,sd)
+df2<-cbind(meanWeek1,meanWeek2,meanWeek3,meanWeek4,sd1,sd2,sd3,sd4,df1)
+write.table(df2,paste0("bugSlurryMouse",sex,"Mice.txt"),sep="\t")
+
+#Pie Chart for all weeks: Paired analysis
+pdf(paste0("pieSlurrybug",sex,"Mice.pdf"))
+for (i in 1:4)
+{
+  meanPercentage<-c(as.numeric(format(df2[8,i],digit=4)),as.numeric(format(df2[9,i],digit=4)),as.numeric(format(df2[10,i],digit=4)))
+  deviations<-c(as.numeric(format(df2[8,i+4],digits = 3)),as.numeric(format(df2[9,i+4],digits = 3)),as.numeric(format(df2[10,i+4],digits = 3)))
+  slices<-paste(meanPercentage,"\u00b1",deviations,"%",sep="")
+  labels<-c("Shared","Only in Slurry","Only in Mouse")
+  labels<-paste(labels,"\n",slices)
+  pie(meanPercentage,labels,col = c("indianred1","blue","orchid"),radius = 0.4,cex=0.8)
+  title(paste0("Average percentage of sequence variants\n present in either mouse or slurry or both","\nWeek= ",i),line=-3,cex=0.3)
+}
+
+dev.off()
+
+#Box plot for the shared:
+
+df_Female<-read.table(paste0(output,"bugSlurryMouseFMice.txt"),sep="\t",header = TRUE)
+df_F<-t(df_Female)
+df_F<-as.data.frame(df_F)
+df_F<-df_F[9:nrow(df_F),]
+df_F$sex<-rep("Female",nrow(df_F))
+splitNames<-sapply(rownames(df_F),function(x){strsplit(x,"week.")[[1]][2]})
+week<-sapply(splitNames,function(x){strsplit(x,"_")[[1]][1]})
+df_F$week<-week
+
+df_male<-read.table(paste0(output,"bugSlurryMouseMMice.txt"),sep="\t",header = TRUE)
+df_M<-t(df_male)
+df_M<-as.data.frame(df_M)
+df_M<-df_M[9:nrow(df_M),]
+df_M$sex<-rep("Male",nrow(df_M))
+splitNames<-sapply(rownames(df_M),function(x){strsplit(x,"week.")[[1]][2]})
+week<-sapply(splitNames,function(x){strsplit(x,"_")[[1]][1]})
+df_M$week<-week
+
+df_all<-rbind(df_F,df_M)
+df_all$sex<-as.factor(df_all$sex)
+
+library(cowplot)
+library(ggsignif)
+theme_set(theme_classic(base_size = 9))
+plot1<-ggplot(df_all,aes(x=week,y=percentBugShared3,color=as.factor(sex)))+geom_boxplot(outlier.shape = NA)+
+  labs(y="Shared (%)",color="")+geom_jitter(shape=16, size =0.4,position = position_jitterdodge(),aes(color=sex))+
+  ylim(0,100)+annotate(geom = "text",x=2,y=70,label="p=0.02",size=2)
+
+plot2<-ggplot(df_all,aes(x=week,y=percentBugSlurry3,color=as.factor(sex)))+geom_boxplot(outlier.shape = NA)+
+  labs(y="Only in slurry (%)",color="")+geom_jitter(shape=16, size =0.4,position = position_jitterdodge(),aes(color=sex))+
+  ylim(0,100)+annotate(geom = "text",x=2,y=60,label="p=0.01",size=2)
+
+plot3<-ggplot(df_all,aes(x=week,y=percentBugMouse3,color=as.factor(sex)))+geom_boxplot(outlier.shape = NA)+
+  labs(y="Only in mouse fecal pellets (%)",color="")+geom_jitter(shape=16, size =0.4,position = position_jitterdodge(),aes(color=sex))+
+  ylim(0,100)
+
+
+pdf("ComparisonOfMaleAndFemaleMice.pdf",width = 9,height = 4)
+plot_grid(plot1,plot2,plot3,nrow=1,ncol=3,labels = c("A","B","C"),label_size =12)
+dev.off()
+
+df_ll1<-df_all[df_all$week==1,]
+df_ll2<-df_all[df_all$week==2,]
+df_ll3<-df_all[df_all$week==3,]
+df_ll4<-df_all[df_all$week==4,]
+
+t.test(df_ll1[df_ll1$sex=="Female",]$percentBugShared3,df_ll1[df_ll1$sex=="Male",]$percentBugShared3)
+t.test(df_ll2[df_ll2$sex=="Female",]$percentBugShared3,df_ll2[df_ll2$sex=="Male",]$percentBugShared3)
+t.test(df_ll3[df_ll3$sex=="Female",]$percentBugShared3,df_ll3[df_ll3$sex=="Male",]$percentBugShared3)
+t.test(df_ll4[df_ll4$sex=="Female",]$percentBugShared3,df_ll4[df_ll4$sex=="Male",]$percentBugShared3)
+
+t.test(df_ll1[df_ll1$sex=="Female",]$percentBugSlurry3,df_ll1[df_ll1$sex=="Male",]$percentBugSlurry3)
+t.test(df_ll2[df_ll2$sex=="Female",]$percentBugSlurry3,df_ll2[df_ll2$sex=="Male",]$percentBugSlurry3)
+t.test(df_ll3[df_ll3$sex=="Female",]$percentBugSlurry3,df_ll3[df_ll3$sex=="Male",]$percentBugSlurry3)
+t.test(df_ll4[df_ll4$sex=="Female",]$percentBugSlurry3,df_ll4[df_ll4$sex=="Male",]$percentBugSlurry3)
+
+t.test(df_ll1[df_ll1$sex=="Female",]$percentBugMouse3,df_ll1[df_ll1$sex=="Male",]$percentBugMouse3)
+t.test(df_ll2[df_ll2$sex=="Female",]$percentBugMouse3,df_ll2[df_ll2$sex=="Male",]$percentBugMouse3)
+t.test(df_ll3[df_ll3$sex=="Female",]$percentBugMouse3,df_ll3[df_ll3$sex=="Male",]$percentBugMouse3)
+t.test(df_ll4[df_ll4$sex=="Female",]$percentBugMouse3,df_ll4[df_ll4$sex=="Male",]$percentBugMouse3)
+
+
+
+
---
+++ MicrobiotaTransferEfficiencyFemaleMaleMice1.R
@@ -0,0 +1,148 @@
+#Farnaz Fouladi
+#11-22-19
+#Comparing the transfer efficiency between male and female mice. 
+#Comparison of Spearman correlation coefficients between male and female mice.
+
+rm(list =ls())
+
+library(ggplot2) 
+library(grid)
+library(gridExtra) 
+
+output<-"/Users/farnazfouladi/Google Drive/MicrobiotaTransfer/output/"
+input<-"/Users/farnazfouladi/Google Drive/MicrobiotaTransfer/input/"
+setwd(output)
+data<-read.table(paste0(output,"svMeta.txt"),sep = "\t",header=TRUE,check.names=FALSE,na.strings = "NA" ,comment.char="")
+finishAbundanceIndex<-which(colnames(data)=="Sample")-1
+#Removing low abundant sv
+sv<-data[,1:finishAbundanceIndex]
+sv<-sv[,colMeans(sv>0)>=0.1]
+myT<-cbind(sv,data[,(finishAbundanceIndex+1):ncol(data)])
+finishAbundanceIndex<-which(colnames(myT)=="Sample")-1
+Donors<-as.character(myT$Donor[myT$Sample.type=="Human.donor"])
+myTH<-myT[myT$Sample.type=="Fecal.slurry",]
+SlurryNames<-myTH$Slurry.ID1
+SlurryNames<-SlurryNames[SlurryNames!="T1.slurry.EAN40"]#For this slurry there is no mouse
+
+#Choose female or male mice
+sex="M"
+myTMouse_S<-myT[myT$Sex==sex & myT$Sample.type=="Mouse.feces",]
+#Removing slurries that were not used for colonization of mice
+Slurries_S<-SlurryNames[SlurryNames %in% as.character(myTMouse_S$Slurry.ID1)]
+
+spearman<-vector()
+rho<-vector()
+time<-vector()
+Slurry<-vector()
+Mouse<-vector()
+outerindex<-1
+
+for (week in 1:4){
+  
+  myTweek<-myTMouse_S[myTMouse_S$Week==week,]
+  
+  for (slurry in Slurries_S){
+    
+    if (slurry %in% myTweek$Slurry.ID1){
+    
+    bugH<-myTH[myTH$Slurry.ID1==slurry,]
+    myTweekSlurry<-myTweek[myTweek$Slurry.ID1==slurry,]
+    
+    for (j in 1:nrow(myTweekSlurry)){
+      
+      abundanceinSlurry<-vector()
+      AbundanceinMice<-vector()
+      index<-1  
+      
+      bugM<-myTweekSlurry[j,]
+      
+      for (i in 1:finishAbundanceIndex){
+        
+        if (!(bugH[,i]==0 & bugM[,i]==0)){
+          abundanceinSlurry[index]<-bugH[,i]
+          AbundanceinMice[index]<-bugM[,i]
+          index<-index+1
+        }
+      }
+      
+      spearman[outerindex]<-cor.test(abundanceinSlurry,AbundanceinMice,method = "spearman")$p.value
+      rho[outerindex]<-cor.test(abundanceinSlurry,AbundanceinMice,method = "spearman")$estimate
+      time[outerindex]<-week
+      Slurry[outerindex]<-slurry
+      Mouse[outerindex]<-as.character(bugM$Sample.ID)
+      outerindex<-outerindex+1
+    }
+  }
+  }
+}
+
+aframe<-data.frame(Slurry,Mouse,time,spearman,rho)
+aframe$Adjustedspearman<-p.adjust(aframe$spearman,method ="BH")
+write.table(aframe,paste0("SlurryMiceTransferSV_",sex,".txt"),sep = "\t",row.names = FALSE)
+
+#Plotting
+myList<-list()
+index<-1
+
+for (i in 1:nrow(aframe)){
+  
+  data<-data.frame(abundanceinSlurry=as.numeric(myT[myT$Sample.type=="Fecal.slurry" & myT$Slurry.ID1==as.character(aframe$Slurry[i]),1:finishAbundanceIndex]),
+                   AbundanceinMice=as.numeric(myT[myT$Sample.type=="Mouse.feces" & myT$Slurry.ID1==as.character(aframe$Slurry[i]) & 
+                                                    myT$Week==aframe$time[i] & !is.na(myT$Week) & myT$Sample.ID==as.character(aframe$Mouse[i]) ,1:finishAbundanceIndex]))
+  data1<-data[!(data$abundanceinSlurry==0 & data$AbundanceinMice==0), ]
+  text1<-paste0("p-value= ",format(aframe$Adjustedspearman[i],digits = 3),"\nrho= ",format(aframe$rho[i],digits = 3),"\nweek= ",aframe$time[i])
+  myPlot<-ggplot(data1,aes(x=abundanceinSlurry,y=AbundanceinMice))+geom_point(size=0.5)+labs(title=text1,x=paste0("Abundance of SVs in the slurry "),y="Abundance of SVs in the paired mouse fecal pellet")+theme_gray(base_size = 6)
+  myList[[index]]<-myPlot
+  index<-index+1
+}
+
+
+#Plot
+myVector<-vector()
+index<-1
+x=1
+while (x<length(myList)){
+  myVector[index]<-x
+  x<-x+9
+  index<-index+1
+}
+pdf(paste0("SlurryMouseTransferPanel2_",sex,".pdf"))
+for (i in myVector){
+  if(i<length(myList)-1){
+    grid.arrange(myList[[i]],myList[[i+1]],myList[[i+2]],myList[[i+3]],myList[[i+4]],myList[[i+5]],myList[[i+6]],myList[[i+7]],myList[[i+8]],ncol=3) 
+  } else {
+    grid.arrange(myList[[i]],myList[[i+1]],ncol=3,nrow=3)
+  }
+}
+dev.off()
+
+
+#Box plot for comparing transfer efficiency between male and female mice
+
+aframe_M<-read.table(paste0(output,"SlurryMiceTransferSV_M.txt"),header = TRUE,sep="\t")
+aframe_M$sex<-rep("Male",nrow(aframe_M))
+aframe_F<-read.table(paste0(output,"SlurryMiceTransferSV_F.txt"),header = TRUE,sep="\t")
+aframe_F$sex<-rep("Female",nrow(aframe_F))
+aframe_all<-rbind(aframe_M,aframe_F)
+aframe_all$time<-factor(aframe_all$time)
+#Comparing rho coeficients between female and male mice:
+theme_set(theme_classic(base_size = 5))
+plot1<-ggplot(aframe_all,aes(x=time,y=rho,color=as.factor(sex)))+geom_boxplot(outlier.shape = NA)+
+  labs(y="Rho",color="")+geom_jitter(shape=16, size =0.2,position = position_jitterdodge(0.1),aes(color=sex))+
+  annotate(geom="text", x=2, y=0.1, label="p=0.04",size=2,
+           color="black")
+  
+
+aframe_all1<-aframe_all[aframe_all$time==1,]
+aframe_all2<-aframe_all[aframe_all$time==2,]
+aframe_all3<-aframe_all[aframe_all$time==3,]
+aframe_all4<-aframe_all[aframe_all$time==4,]
+
+t.test(aframe_all1[aframe_all1$sex=="Female",]$rho,aframe_all1[aframe_all1$sex=="Male",]$rho)
+t.test(aframe_all2[aframe_all2$sex=="Female",]$rho,aframe_all2[aframe_all2$sex=="Male",]$rho)
+t.test(aframe_all3[aframe_all3$sex=="Female",]$rho,aframe_all3[aframe_all3$sex=="Male",]$rho)
+t.test(aframe_all4[aframe_all4$sex=="Female",]$rho,aframe_all4[aframe_all4$sex=="Male",]$rho)
+
+pdf("ComparisonOfMaleAndFemaleMice_1.pdf",width = 3,height = 3)
+print(plot1)
+dev.off()
